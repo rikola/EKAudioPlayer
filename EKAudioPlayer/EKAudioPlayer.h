f0bafe012c77400b77dd1aa811bac24f44850dd8
Eric <rikola2@gmail.com>
2016-02-18 16:10:36 -0600

    Initial Commit

---
+++ EKAudioPlayer/EKAudioPlayer.h
@@ -0,0 +1,19 @@
+//
+//  EKAudioPlayer.h
+//  EKAudioPlayer
+//
+//  Created by Eric Koehler  on 2/18/16.
+//  Copyright © 2016 Eric Koehler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EKAudioPlayer.
+FOUNDATION_EXPORT double EKAudioPlayerVersionNumber;
+
+//! Project version string for EKAudioPlayer.
+FOUNDATION_EXPORT const unsigned char EKAudioPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EKAudioPlayer/PublicHeader.h>
+
+
